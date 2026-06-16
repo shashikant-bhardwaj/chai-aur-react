4f148403ab886a48b75de9c47a1891d1830e8286
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-06-16 18:21:43 +0530

    redux toolkit project

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todo from './Components/Todo'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1>learn about redux tool kit</h1>
+      <AddTodo/>
+      <Todo/>
+     
+    </>
+  )
+}
+
+export default App
