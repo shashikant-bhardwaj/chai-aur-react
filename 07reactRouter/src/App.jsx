c2bde1a556398e45a90e05333908ece88ca8e856
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-06-17 03:31:46 +0530

    react router project

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Layout from './components/Layout'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    
+    <Header/>
+    <Home/>
+    <Footer/>
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,14 +2,51 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Header from './components/Header/Header.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import { Route } from 'react-router-dom'
+import User from './components/User/User.jsx'
+
 
 
 const router = createBrowserRouter([
   {
-    path: '/'
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+         {
+        path: "Contact",
+        element: <Contact/>
+      },
+            {
+        path: "User/:userid",
+        element: <User/>
+      }
+    ]
   }
 ])
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path ='/' Element = {<Layout/>}>
+//     <Route path ='' element ={<Home/>}/>
+//     <Route path ='About' element ={<About/>}/>
+//     <Route path ='Contact' element ={<Contact/>}/>
+//     </Route>
+//   )
+// )
 
 
 
