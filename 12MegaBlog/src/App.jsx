e73725d15679b1902a4c9f1c7256df8447c25e26
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-06-18 13:57:13 +0530

    MegaBlog Project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+console.log(import.meta.env.VITE_APPWRITE_URL)
+console.log(import.meta.env.VITE_DB_PASSWORD)
+  return (
+    <>
+    <h1>A blog app in appwrite</h1>
+    </>
+  )
+}
+
+export default App
